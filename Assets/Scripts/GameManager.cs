255cfd643aeb8c030d2aea69c8e73fd723fc3f90
Eric Veciana <eveciana21@gmail.com>
2023-07-15 16:22:33 -0600

    finished menu screen and implemented button actions with arrow firing at them

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,13 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Animator _animator;
+
+    private void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "Menu")
+        {
+            _animator.SetLayerWeight(0, 0f);
+            _animator.SetLayerWeight(1, 0f);
+            _animator.SetLayerWeight(2, 0f);
+            _animator.SetLayerWeight(3, 1f);
+        }
+    }
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(1);
         }
     }
 
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private PlayableDirector _playDirector, _optionsDirector, _quitDirector;
+
 
     public void PlayButton()
     {
-        SceneManager.LoadScene(2); // LEVEL ONE
+        StartCoroutine(Play());
+    }
+
+    public void OptionsButton()
+    {
+        StartCoroutine(Options());
     }
 
     public void QuitButton()
     {
-        Application.Quit();
+        StartCoroutine(Quit());
+    }
+
+    IEnumerator Play()
+    {
+        _playDirector.Play();
+        yield return new WaitForSeconds(2);
+        //SceneManager.LoadScene(2); // LEVEL ONE
+    }
+
+    IEnumerator Options()
+    {
+        _optionsDirector.Play();
+        yield return new WaitForSeconds(2);
+        // SceneManager.LoadScene(2); // LEVEL ONE
+    }
+    IEnumerator Quit()
+    {
+        _quitDirector.Play();
+        yield return new WaitForSeconds(2);
+        //Application.Quit();
     }
 }
